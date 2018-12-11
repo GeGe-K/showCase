1d6b36d052ab59fd8527d93e65f7b4c1e80fa964
GeGe-K <gloriagivondo@gmail.com>
2018-12-11 15:27:44 +0300

    ran tests for the clsaaes photo and category

--- pics/tests.py
+++ pics/tests.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
+from .models import Category,Photo
 
 # Create your tests here.
+class PhotoTestClass(TestCase):
+
+    # Set up method 
+    def setUp(self):
+        
+        self.category = Category(name = 'Metal Box')
+        self.category.save_category()
+        self.photo = Photo(photo='photo.jpeg', name = 'photo', description = 'testing photo', category = self.category)
+
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.photo, Photo))
+
+    # Testing save method
+    def test_save_method(self):
+        self.photo.save_photo()
+        photo = Photo.objects.all()
+        self.assertTrue(len(photo)>0)
+
+    # Testing delete method
+    def test_delete_method(self):
+        self.photo.save_photo()
+        self.photo.delete_photo()
+
+
+class CategoryTestClass(TestCase):
+
+    #Set up method
+    def setUp(self):
+        self.category = Category(name='Sports')
+
+    #Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category, Category))
+
+    # Testing save method
+    def test_save_method(self):
+
+        self.category.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+
+    # Testing delete method
+    def test_delete_method(self):
+
+        self.category.save_category()
+        self.category.delete_category()
