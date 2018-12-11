b52602701c75b004879955a76ffed1910a373202
GeGe-K <gloriagivondo@gmail.com>
2018-12-11 15:59:27 +0300

    added urls for search, photo and category

--- pics/urls.py
+++ pics/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns=[
     url('^$',views.index,name = 'index'),
-    # url(r'^search', views.search_photo, name='search_photo'),
-    # url(r'^photo/(\d+)', views.photo, name='photo'),
-    # url(r'category/(\d+)', views.filter_by_category, name='category'),
+    url(r'^search', views.search_photo, name='search_photo'),
+    url(r'^photo/(\d+)', views.photo, name='photo'),
+    url(r'category/(\d+)', views.filter_by_category, name='category'),
 ]
